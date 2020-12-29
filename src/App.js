be9a2758af36b82999373d46c796b97210f765ad
angshuman-biswas <23angshu@gmail.com>
2020-12-29 19:06:10 +0530

    App made mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import PostList from './components/PostList';
 
 
 function App() {
-
-  useEffect(() => {
-  }, []);
 
   return (
     <div className="App">
